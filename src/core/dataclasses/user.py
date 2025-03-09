a6078b2e7bbcefa7771774d574e3b26f18fa72f3
tuan.ngo <tuan.ngo@paradox.ai>
2025-03-09 22:57:16 +0700

    fix doc

--- src/core/dataclasses/user.py
+++ src/core/dataclasses/user.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python
 #
-# Copyright (C) 2024 paradox.ai
+# Copyright (C) 2025 paradox.ai
 #
-# Release: 2.4.5
+# Release: 1.0.0
 # @link olivia.paradox.ai
 #
 __author__ = "tuan.ngo"
--- src/tests/order/domain/actions/__init__.py
+++ src/tests/order/domain/actions/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2025 paradox.ai
 #
-# Release: 2.4.9
+# Release: 1.0.0
 # @link olivia.paradox.ai
 #
 __author__ = "tuan.ngo"
